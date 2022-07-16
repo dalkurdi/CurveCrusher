e545c59441b05bc16ffffc7dec131293149c825e
thieryw <williamalexthiery@hotmail.fr>
2022-07-16 14:37:33 +0200

    create deploy ci script so that user can name his landing page branch the way he wants

--- template/src/bin/runScripts.ts
+++ template/src/bin/runScripts.ts
@@ -1,9 +1,11 @@
 import { createRouterFile } from "./createRouterFile";
 import { clean } from "./clean";
 import { setPackageJsonHomepage } from "./setPackageJsonHomepage";
+import { createCi } from "./createCi";
 
 (async () => {
 	await setPackageJsonHomepage();
 	await createRouterFile();
+	createCi();
 	clean();
 })();
